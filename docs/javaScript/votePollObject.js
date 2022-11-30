8f84a9d16cb4497a2ffb825e18798c1a3d663ae9
benberton <91513576+benberton@users.noreply.github.com>
2022-11-30 12:49:41 -0800

    pairwise comparison finished, a tie when two candidates go head to head is represented with a 0.5

--- docs/javaScript/votePollObject.js
+++ docs/javaScript/votePollObject.js
@@ -62,17 +62,67 @@ class votePoll{
             for (let j = 0; j < values[i].voteRanking.length; ++j)
                 voteCountArray[this.candidates.indexOf(values[i].voteRanking[j])][0] += (values[i].voteCount * (maxPoints - j))
 
-         //sorting array by the number of "points"
-         voteCountArray.sort(sortFunction)
-         return voteCountArray
+        //sorting array by the number of "points"
+        voteCountArray.sort(sortFunction)
+        return voteCountArray
     }
 
+    getPairwiseComparison()
+    {
+        //creating 2d array to hold both the candidate name and their total "points"
+        let voteCountArray = new Array(this.candidates.length)
+        for (let i = 0; i < voteCountArray.length; ++i)
+        { 
+            voteCountArray[i] = new Array(2)
+            voteCountArray[i][0] = 0
+            voteCountArray[i][1] = this.candidates[i]
+        }
+
+        //getting votes
+        const values = Array.from(this.voteMap.values())
+
+        for (let i = 0; i < this.candidates.length - 1; ++i)
+        {
+            for (let j = i + 1; j < this.candidates.length; ++j)
+            {
+                let totalA = 0
+                let totalB = 0
+                for (let k = 0; k < values.length; ++k)
+                {
+                    let voteCandidates = values[k].voteRanking
+                    if(voteCandidates.indexOf(this.candidates[i]) < voteCandidates.indexOf(this.candidates[j]))
+                    {
+                        totalA += values[k].voteCount
+                    }else
+                        totalB += values[k].voteCount
+                }
+
+                //if the total votes won for candidate A is greater than candidate B, then candidate A wins
+                if (totalA == totalB)
+                {
+                    voteCountArray[i][0] += 0.5
+                    voteCountArray[j][0] += 0.5
+                }else if (totalA > totalB)
+                    voteCountArray[i][0]++
+                else
+                    voteCountArray[j][0]++
+            }
+        }
+        
+        //sorting array by the number of "points"
+        voteCountArray.sort(sortFunction)
+
+        return voteCountArray
+    }
+    
     printVotes()
     {
         const values = Array.from(this.voteMap.values())
         console.log(values)
     }
 }
+
+
 
 //sorts 2d array
 function sortFunction(a, b)
@@ -99,5 +149,5 @@ newPoll.addVote(fourthVote)
 
 console.log(newPoll.getPluralityResult())
 console.log(newPoll.getBoardaCountResult())
-
+console.log(newPoll.getPairwiseComparison())
 newPoll.printVotes()
