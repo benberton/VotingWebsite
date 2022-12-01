be0a2bf6088d99e9e82474723fcce9466dcd3a7c
benberton <91513576+benberton@users.noreply.github.com>
2022-12-01 14:43:37 -0800

    basic vote screen made, submit button not written yet

--- docs/javaScript/main.js
+++ docs/javaScript/main.js
@@ -1,5 +1,38 @@
+
+let newPoll = new votePoll(["Mike","Bibby","James"])
+
+
 document.addEventListener("DOMContentLoaded", ()=>{
+    addCandidatesToScreen()
     //creates javascript for the dragable object
     slist(document.getElementById("sortlist"));
 
 })
+
+function addCandidatesToScreen()
+{
+    for (let i = 0; i < newPoll.candidates.length; ++i)
+    {
+        const candidateEl = document.createElement("li")
+        candidateEl.innerHTML = newPoll.candidates[i]
+        document.getElementById("sortlist").appendChild(candidateEl)
+    }
+}
+
+
+let firstVote = new Vote(["Mike","Bibby","James"])
+let secondVote = new Vote(["Mike","Bibby","James"])
+let thirdVote = new Vote(["Bibby","Mike","James"])
+let fourthVote = new Vote(["Bibby","James","Mike"])
+
+
+newPoll.addVote(firstVote)
+newPoll.addVote(secondVote)
+newPoll.addVote(thirdVote)
+newPoll.addVote(fourthVote)
+
+// console.log(newPoll.getPluralityResult())
+// console.log(newPoll.getBoardaCountResult())
+// console.log(newPoll.getPairwiseComparison())
+console.log(newPoll.pluralityEliminationResult())
+newPoll.printVotes()
--- docs/javaScript/votePollObject.js
+++ docs/javaScript/votePollObject.js
@@ -182,8 +182,6 @@ class votePoll{
     }
 }
 
-
-
 //sorts 2d array
 function sortFunction(a, b)
 {
@@ -194,21 +192,3 @@ function sortFunction(a, b)
         return (a[0] > b[0]) ? -1 : 1;
     }
 }
-let newPoll = new votePoll(["Mike","Bibby","James"])
-
-let firstVote = new Vote(["Mike","Bibby","James"])
-let secondVote = new Vote(["Mike","Bibby","James"])
-let thirdVote = new Vote(["Bibby","Mike","James"])
-let fourthVote = new Vote(["Bibby","James","Mike"])
-
-
-newPoll.addVote(firstVote)
-newPoll.addVote(secondVote)
-newPoll.addVote(thirdVote)
-newPoll.addVote(fourthVote)
-
-// console.log(newPoll.getPluralityResult())
-// console.log(newPoll.getBoardaCountResult())
-// console.log(newPoll.getPairwiseComparison())
-console.log(newPoll.pluralityEliminationResult())
-newPoll.printVotes()
