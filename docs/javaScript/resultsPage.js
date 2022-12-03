d2fffeba67d31dd992fa668e34d09a6b67cd8801
benberton <91513576+benberton@users.noreply.github.com>
2022-12-02 22:49:23 -0800

    animation works for label of plurality method

--- docs/javaScript/resultsPage.js
+++ docs/javaScript/resultsPage.js
@@ -4,3 +4,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
     console.log("loaded")
     
 })
+
+
+function resultClicked(labelID,contentID)
+{
+    console.log(labelID)
+    let container = document.getElementById(labelID)
+    let content = document.getElementById(contentID)
+    if (container.classList.contains("resultLabelClicked"))
+    {
+        container.classList.remove("resultLabelClicked")
+        content.classList.add("hidden")
+    }else
+    {
+        container.classList.add("resultLabelClicked")
+        content.classList.remove("hidden")
+    }
+        
+
+    console.log(labelID)
+}
